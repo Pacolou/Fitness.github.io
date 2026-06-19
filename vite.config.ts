8be60997917e1061f642edcc9e5be1c89ee06321
Pacolou <matthew.loureiro@gmail.com>
2026-06-19 08:11:46 -0700

    Set Vite base path for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
   plugins: [react()],
+  base: "/Fitness.github.io/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
